081f09c7f04ec72c5181561fdd89458a166c4ce6
Thedd <mixailmar4uk78@gmail.com>
2026-07-03 21:58:28 +0300

    ref: some ref

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import { setupApp } from './setup-app';
+import { SETTINGS } from './settings/config';
 
 // создание приложения
 const app = express();
 setupApp(app);
 
-// process — это глобальный объект в Node.js, который содержит информацию о текущем процессе выполнения.
-// process.env — это объект, содержащий все переменные окружения, доступные вашему приложению.
-const PORT = process.env.PORT || 5001;
+const PORT = SETTINGS.PORT;
 
 // ф-ия listen - запускает сервер и начинает прослушивать входящие запросы на указанном порту.
 app.listen(PORT, () => {
